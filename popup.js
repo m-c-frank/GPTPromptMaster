11249cbce13a3cb3d13ec657795f5130196c702d
Martin Christoph Frank <martin.frank@hs-augsburg.de>
2023-02-25 17:36:58 +0100

    removed disable button

--- popup.js
+++ popup.js
@@ -1,21 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var disableButton = document.getElementById('disable-button');
     var settingsButton = document.getElementById('settings-button');
     var aboutButton = document.getElementById('about-button');
-
-    var disableButton = document.getElementById('disable-button');
-
-    disableButton.addEventListener('click', function () {
-        chrome.storage.local.get(['disabled'], function (result) {
-            var isDisabled = result.disabled;
-
-            chrome.storage.local.set({ 'disabled': !isDisabled }, function () {
-                var buttonText = isDisabled ? 'Disable' : 'Enable';
-                disableButton.textContent = buttonText;
-            });
-        });
-    });
-
 
     settingsButton.addEventListener('click', function () {
         chrome.tabs.create({ url: chrome.runtime.getURL('settings.html') });
